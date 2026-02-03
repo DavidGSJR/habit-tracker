8064cb5b4f3c46bd4c1ede1caea867ea5ecf19b3
David <davidjunior0805@gmail.com>
2026-02-03 10:02:56 -0300

    feat: init components header/footer/layout

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="bg-white border-t-2 p-4">
+  <p class="text-center">
+      Criado por David Junior. O código fonte está no <a href="https://github.com/DavidGSJR/habit-tracker.git" class="underline">GitHub</a>
+  </p>
+</footer>
