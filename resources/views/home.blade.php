22d16ea81291321e289177f031389ad27fc93b4f
David <davidjunior0805@gmail.com>
2026-02-04 18:08:35 -0300

    feat(Auth): implementação login e logica autenticação

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,6 +3,10 @@
         <h1>
             Veja seus habitos ganharem vida
         </h1>
+
+        @auth
+            <p>Bem vindo(a), {{auth()->user()->name}}!</p>
+        @endauth
     </main>
 
 
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,9 @@ use App\Http\Controllers\Auth\LoginController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\SiteController;
 
+//Site
 Route::get('/', [SiteController::class, 'index']);
 
+//Login
 Route::get('/login', [LoginController::class, 'index']);
+Route::post('/login', [LoginController::class, 'authenticate']);
